a80ed5052e72f420abb7a470e6300d3f8089ecd8
Kizhan Mohammed <kizhan.m.kareem@gmail.com>
2021-04-02 19:22:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
 // Write your solution here!
 let pets = ["Milo", "Otis", "Garfield"];
 
-Function append (pets) {
   pets.push("Odie");
-}
-Function prepend (pets) {
+
   pets.unshift("Odie");
-}
-Function removeLast(pets) {
+
   pets.pop("")
-}
-Function removeFirst(pets){
+
   pets.shift("")
-}
-append (pets);
-prepend (pets);
-removeLast(pets);
-removeFirst(pets)
+
